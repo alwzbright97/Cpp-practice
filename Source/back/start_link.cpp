9cf032271b12c7c311bfe7805a99d54387122c79
alwzbright97 <alwzbright97@gmail.com>
2022-05-11 18:02:42 +0900

    start start_link.cpp

---
+++ Source/back/start_link.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int N, stat[20][20], player[20];
+int start = 0, link = 0;
+void dfs()
+{
+    if (/* condition */)
+    {
+        /* code */
+    }
+}
+int main()
+{
+    std::cout.tie(NULL);
+    std::ios::sync_with_stdio(false);
+}
