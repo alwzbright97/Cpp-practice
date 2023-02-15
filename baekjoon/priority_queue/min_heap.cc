fae982febff0ff3ef5a170a403713e3e3a62d8fa
Bright <alwzbright97@gmail.com>
2023-02-15 23:29:53 +0900

    [Add] Add priority queue solution - 1

---
+++ baekjoon/priority_queue/min_heap.cc
@@ -0,0 +1,95 @@
+#include <iostream>
+
+constexpr int MAX = 100000;
+
+int size = 0, arr[MAX];
+
+/* void print_status() {
+  for (int i = 0; i < size; i++) {
+    std::cout << arr[i] << " ";
+  }
+  std::cout << "\n";
+
+  return;
+} */
+
+void swap(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void insert(int x) {
+  int child = size;
+  int parent = (child - 1) >> 1;
+  arr[size++] = x;
+
+  while ((parent >= 0) && (arr[child] < arr[parent])) {
+    swap(&arr[child], &arr[parent]);
+    child = parent;
+    parent = (child - 1) >> 1;
+  }
+
+  // print_status();
+}
+
+int pop(void) {
+  if (!size)
+    return 0;
+
+  int out = arr[0];
+  arr[0] = 0;
+  swap(&arr[0], &arr[--size]);
+
+  int ptr = 0;
+
+  while (ptr < size) {
+    int left = (ptr << 1) + 1;
+    int right = (ptr << 1) + 2;
+
+    if (left < size || right < size) {
+      int tmp = ptr;
+      if ((left < size) && arr[left] < arr[tmp]) {
+        tmp = left;
+      }
+      if ((right < size) && arr[right] < arr[tmp]) {
+        tmp = right;
+      }
+
+      if (tmp == ptr)
+        break;
+      swap(&arr[tmp], &arr[ptr]);
+      ptr = tmp;
+    } else {
+      break;
+    }
+
+    // std::cout << "\tsize: " << size << "\n"
+    //           << "\tptr: " << size << "\n"
+    //           << "\tleft: " << left << "\n"
+    //           << "\tright: " << right << "\n";
+  }
+
+  // print_status();
+  return out;
+}
+
+int main() {
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+  std::ios::sync_with_stdio(false);
+
+  int N, x;
+  std::cin >> N;
+  for (int i = 0; i < N; i++) {
+    std::cin >> x;
+
+    if (x) {
+      insert(x);
+    } else {
+      std::cout << pop() << "\n";
+    }
+  }
+
+  return 0;
+}
